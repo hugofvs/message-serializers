3d3c78548f051c4e4e2510c5e2a3f2076e9b4fd0
Hugo Silva <hugo.f.v.silva@gmail.com>
2017-11-26 22:34:31 +0000

    Add view to handle messagepack

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from message_formats.small_proto3_pb2 import User
+import msgpack
 
 
 app = Flask(__name__)
@@ -7,12 +8,18 @@ app = Flask(__name__)
 
 @app.route('/test/json/', methods=['POST'])
 def test_json_view():
-    username = request.get_json()['username']
-    return username, 200
+    # username = request.get_json()['username']
+    return 'Ok', 200
 
 
 @app.route('/test/protobuf/', methods=['POST'])
 def test_protobufs_view():
-    u = User()
-    u.ParseFromString(request.get_data().replace(b'\r\n', b'\n'))
-    return u.username, 200
+    # u = User()
+    # u.ParseFromString(request.get_data().replace(b'\r\n', b'\n'))
+    return 'Ok', 200
+
+
+@app.route('/test/messagepack/', methods=['POST'])
+def test_messagepack_view():
+    # user_dict = msgpack.unpackb(request.get_data(), encoding='utf-8')
+    return 'Ok', 200
